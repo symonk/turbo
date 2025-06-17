9039ac64bb4dda942281ed48fc2f1477bcac2223
symonk <jackofspaces@gmail.com>
2025-06-17 22:38:09 +0100

    Remove concept of `Generic` pool

--- turbo.go
+++ turbo.go
@@ -4,4 +4,5 @@ import "github.com/symonk/turbo/pool"
 
 // WorkerPool manages a set of worker goroutines to execute tasks
 // submitted concurrently
-type WorkerPool[T any] = pool.WorkerPool[T]
+type WorkerPool[T any] = pool.WorkerPool
+
